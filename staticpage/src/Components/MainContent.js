84dc022abd31ca606be732a6ebe0632e158f448e
Rohan Arora <rohan.arora.2000.18@gmail.com>
2022-05-07 17:09:53 +0530

    completed a  single static project app.

--- staticpage/src/Components/MainContent.js
+++ staticpage/src/Components/MainContent.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default function MainContent(){
     return(
-        <h1 className="MainContentText">Main Content Goes Here</h1>
+        <div className="mainContentContainer">
+        <h1 className="MainContentHeading">Fun Facts About React</h1>
+        <ul>
+            <li>Was First Released in 2013</li>
+            <li>Was Orginally Created by Jordan Walke</li>
+            <li>Has Well Over 100k stars on GitHub</li>
+            <li>Is Maintained by Meta</li>
+            <li>Powers thousands of enterpise apps,including mobile apps</li>
+        </ul>
+        </div>
     )
 }
--- staticpage/src/Components/Navbar.js
+++ staticpage/src/Components/Navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import logo from '.././logo.svg';
 
 export default function Navbar(){
     return(
-        <h1 className="NavbarText">Navbar Goes Here</h1>
+        <div className="navbarContainer">
+            <div className="navbarbrand">
+                <img src={logo} className="logoImage"></img>
+                <h1 className="NavbarBrandText">ReactFacts</h1>
+            </div>
+        
+        <h1 className="NavbarText">React Basic Project 1</h1>
+        </div>
     )
 }
