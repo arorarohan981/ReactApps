a18fee7ea374aea417c86b3c09e4a49eff3e2106
Rohan Arora <rohan.arora.2000.18@gmail.com>
2022-08-04 14:50:55 +0530

    Completed Header Component.

--- restarauntwebsite/src/container/Header/Header.jsx
+++ restarauntwebsite/src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import {images} from '../../constants';
+import SubHeading from "../../components/SubHeading/SubHeading";
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the New Flavour"/>
+      <h1 className="app__header-h1">The Key To Fine Dining </h1>
+      <p className="p__opensans" style={{ margin : '2rem 0'}}> Experience the Flavours and Hospitality of Germany</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app__header-img">
+      <img src={images.welcome} alt='Header Image'/>
+    </div>
+    
   </div>
 );
 
